b056d64fd261816396d5f55bfef78a91b87e32a7
ulrich <ulm_ulm@hotmail.com>
2018-11-20 19:23:01 +0100

    JS slideshow updated to manage arrowsNav when resizing window

--- public/js/slideshow.js
+++ public/js/slideshow.js
@@ -1,20 +1,58 @@
 'use strict';
 
 class Slideshow {
-    constructor(slidesVue) {
-        this.slidesVue = slidesVue;
-        this.container = document.querySelector('.slideshow-container');
-        this.slideshow = document.querySelector('.slideshow');
-        this.slideElts = document.querySelectorAll('.slide-elt');
-        this.ratio = this.slideElts.length / this.slidesVue;
+    constructor(type) {
+      //  this.slidesVue = slidesVue;
+        this.type = type;
+        this.initType(type);
+       // this.container = document.querySelector('.slideshow-container');
+       // this.slideshow = document.querySelector('.slideshow');
+       // this.slideElts = document.querySelectorAll('.slide-elt');
         this.currentIndex = 0;
+        this.testWindowWidth();
+        window.addEventListener('resize', this.testWindowWidth.bind(this));
+    }
+
+    initType(type) {
+        switch (type) {
+            case 'laptop':
+                this.container = document.querySelector('.laptop-slideshow-container');
+                this.slideshow = document.querySelector('.laptop-slideshow');
+                this.slideElts = document.querySelectorAll('.laptop-slide-elt');
+                break;
+            case 'mobile':
+                this.container = document.querySelector('.mobile-slideshow-container');
+                this.slideshow = document.querySelector('.mobile-slideshow');
+                this.slideElts = document.querySelectorAll('.mobile-slide-elt');
+                break;
+        }
     }
 
     init() {
-        this.setSlideshowWidth();
-        this.setSlideEltWidthAndPadding();
         this.buildNavigation();
         this.hideOrShowArrowNav();
+    }
+
+    testWindowWidth()
+    {
+        if (window.innerWidth <= 768 && this.type === 'laptop') {
+            this.slidesVue = 1;
+        } else if (window.innerWidth > 768 && this.type === 'laptop') {
+            this.slidesVue = 2;
+        }
+        if (this.type === 'mobile' && window.innerWidth <= 384) {
+            this.slidesVue = 1;
+        } else if (this.type === 'mobile' && window.innerWidth > 384 && window.innerWidth <= 850) {
+            this.slidesVue = 2
+        } else if (this.type === 'mobile' && window.innerWidth > 850) {
+            this.slidesVue = 4
+        }
+        this.ratio = this.slideElts.length / this.slidesVue;
+        this.setSlideshowWidth();
+        this.setSlideEltWidthAndPadding();
+        if (this.leftArrow !== undefined) {
+            this.moveTo(0);
+        }
     }
 
     setSlideshowWidth() {
@@ -24,8 +62,6 @@ class Slideshow {
     setSlideEltWidthAndPadding() {
         for (let i = 0; i < this.slideElts.length; i++) {
             this.slideElts[i].style.width = ((100 / this.slidesVue) / this.ratio) + '%';
-            if (i % 2 === 0) this.slideElts[i].style.paddingRight = "5px";
-            else this.slideElts[i].style.paddingLeft = "5px";
         }
     }
 
@@ -38,7 +74,7 @@ class Slideshow {
     buildNavigation() {
       this.leftArrow = this.buildDiv('left-arrow');
       this.rightArrow = this.buildDiv('right-arrow');
-      const arrowContainer = document.querySelector('.arrow-container');
+      const arrowContainer = document.querySelector('.' + this.type + '-arrow-container');
       arrowContainer.appendChild(this.leftArrow);
       arrowContainer.appendChild(this.rightArrow);
       this.leftArrow.addEventListener('click', this.prev.bind(this));
@@ -46,12 +82,12 @@ class Slideshow {
     }
 
     next() {
-        console.log(this.currentIndex);
+        console.log(this.type + ' : ' + this.currentIndex);
         this.moveTo(this.currentIndex + this.slidesVue);
     }
 
     prev() {
-        console.log(this.currentIndex);
+        console.log(this.type + ' : ' + this.currentIndex);
         this.moveTo(this.currentIndex - this.slidesVue);
     }
 
@@ -61,6 +97,8 @@ class Slideshow {
         this.currentIndex = index;
         this.hideOrShowArrowNav();
     }
+
+
 
     hideOrShowArrowNav() {
         if (this.slidesVue === this.slideElts.length) {
@@ -80,6 +118,9 @@ class Slideshow {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-   const slideshow = new Slideshow(2);
-   slideshow.init();
+    const laptopSlideshow = new Slideshow('laptop');
+    laptopSlideshow.init();
+    const mobileSlideshow = new Slideshow('mobile');
+    mobileSlideshow.init();
+
 });
